c7d095e6bc5297b409c3aa1f98551f762c24a8ac
Md Shayon <mdshayon0@gmail.com>
2020-08-17 15:53:36 -1200

    logout system is made

--- admin/logout.php
+++ admin/logout.php
@@ -1,1 +1,17 @@
+<?php
+include('config.php');
 
+// session_start — Start new or resume existing session
+session_start();
+
+
+// The session_unset() function frees all session variables currently registered.
+session_unset();
+
+
+// session_destroy — Destroys all data registered to a session
+session_destroy();
+
+
+header("location: {$hostname}/admin/");
+?>
