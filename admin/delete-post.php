a032eebb697c37776123cf3b3f7315808e913fff
Md Shayon <mdshayon0@gmail.com>
2020-08-20 05:50:29 -1200

    backend is done, delete file from upload folder, secure folder

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -7,10 +7,19 @@ echo $post_id;
 echo $cat_id;
 
 
+// DELETE IMAGE FROM FOLDER
+$sql1 = "SELECT * FROM post WHERE post_id=$post_id";
+$result = mysqli_query($conn, $sql1) or die("query failed : select");
+$row = mysqli_fetch_assoc($result);
+unlink('upload/' . $row['post_img']);
+
+
 // DELETE FROM `post` WHERE 0
 $sql = "DELETE FROM post WHERE post_id=$post_id;";
 $sql .= "UPDATE category SET post= post-1 WHERE category_id={$cat_id}";
 // WE NEED TO GIVE AN SEMI COLON {;} IN ORDER TO WRITE MULTIPLE QQUERY
+
+
 
 
 
