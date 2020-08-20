afa9672f50cf55f683dea9473ca527e53c46399f
Md Shayon <mdshayon0@gmail.com>
2020-08-20 05:18:01 -1200

    Everything working perfectly except pagination for category and users

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -1,5 +1,23 @@
 <?php 
 include("config.php");
 $post_id = $_GET['id'];
-// $cat_id = 
+$cat_id = $_GET['catid'];
+
+echo $post_id;
+echo $cat_id;
+
+
+// DELETE FROM `post` WHERE 0
+$sql = "DELETE FROM post WHERE post_id=$post_id;";
+$sql .= "UPDATE category SET post= post-1 WHERE category_id={$cat_id}";
+// WE NEED TO GIVE AN SEMI COLON {;} IN ORDER TO WRITE MULTIPLE QQUERY
+
+
+
+if(mysqli_multi_query($conn, $sql)){
+    header("location: {$hostname}/admin/post.php");
+}else{
+    echo "<div class='alert alert-danger'>Query failed</div>";
+}
+
 ?>
